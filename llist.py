926ce62b36507fc216ddeb6ccbe83e7c3cb9df1c
YoannMis <ymisericordia@gmx.com>
2024-08-06 18:00:23 +0200

    update llist.py

--- llist.py
+++ llist.py
@@ -13,29 +13,62 @@ class Node:
 class Llist:
     def __init__(self) -> None:
         self.head = None
-        self.end = None
+        self.tail = None
 
     # définir la méthode magique __str__() pour afficher la liste chaînée
     def __str__(self) -> str:
-        return self.print_llist()
+        return f"[{', '.join((str(element) for element in self.list_from_llist()))}]"
 
     # définir la méthode magique __len__() pour renvoyer le nombre d'éléments contenus dans la liste chaînée
     def __len__(self) -> int:
-        pass
+        return len(self.list_from_llist())
 
-    # définir la méthode print_llist() qui permet de récupérer la llist sous forme de str afin de pouvoir l'imprimer
-    def print_llist(self):
+    # définir la méthode print_list_from_llist() qui permet de transformer la liste chaînée en liste standard
+    def list_from_llist(self):
         llist = []
         current_node = self.head
         while current_node is not None:
-            llist.append(str(current_node.value))
+            # llist.append(str(current_node.value))
+            llist.append(current_node.value)
             # print(llist)  # print de vérification
             current_node = current_node.next_link
-        return f"[{', '.join(llist)}]"
+        return llist
 
     # définir la méthode append() pour ajouter un élément à la fin de la liste
+    def append(self, value: int|str) -> None:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            # current_node = self.head
+            # while current_node.next_link is not None:
+            #     current_node = current_node.next_link
+            # current_node.next_link = new_node
+            last_node = self.tail
+            self.tail = new_node
+            last_node.next_link = self.tail
 
     # définir la méthode remove_at() qui retire un élément à l'index donné
+    def remove_at(self, index: int) -> None:
+        if self.head is None:
+            print("Linked list empty. Cannot remove an element.")
+        elif index >= self.__len__():
+            print("List index out of range.")
+        else:
+            current_node = self.head
+            index_count = 0
+            while current_node.next_link is not None and index_count < index:
+                previous_node = current_node
+                current_node = current_node.next_link
+                index_count += 1
+            if current_node == self.head:
+                self.head = self.tail = None
+            elif current_node == self.tail:
+                previous_node.next_link = None
+                self.tail = previous_node
+            else:
+                previous_node.next_link = current_node.next_link
+            del current_node
 
     # définir la méthode insert() qui insère un élément à l'index donné
 
@@ -55,13 +88,37 @@ if __name__ == "__main__":
     node2 = Node(30)
     node3 = Node(50)
     llist = Llist()
+    print(llist)
+    llist.remove_at(3)
+    print(llist)
 
-    llist.head = node1
-    node1.next_link = node2
-    node2.next_link = node3
+    # llist.head = node1
+    # node1.next_link = node2
+    # node2.next_link = node3
 
     # print(node1.value)
     # print(node1.next_link)
     # print(llist.head.value)
     # print(llist.head.next_link)
+
+    llist.append(node1.value)
+    print(llist.tail.value)
+    llist.append(node2.value)
+    print(llist.tail.value)
+    llist.append(node3.value)
+    print(llist.tail.value)
     print(llist)
+    print(len(llist))
+    # llist.remove_at(0)
+    # print(llist)
+    # print(llist.head)
+    # llist.remove_at(1)
+    # print(llist.tail.value)
+    # print(llist)
+    llist.remove_at(2)
+    print(llist)
+    print(llist.tail.value)
+    # llist.remove_at(3)
+    # print(llist)
+    # llist.remove_at(4)
+    # print(llist)
